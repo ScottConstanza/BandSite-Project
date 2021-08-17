dbb7fd9090845716d20f05a4e456f3691ed34e2a
Um Scottie <samuraimangu@samuraiangusmbp.home>
2021-08-16 20:44:40 -0400

    fixed up the timestamp issue

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,28 +1,51 @@
-let defaultComment = [
-    {
-        name: 'Connor Walton',
-        date: '02/17/2021',
-        text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-    },
+// let defaultComment = [
+//     {
+//         name: 'Connor Walton',
+//         date: '02/17/2021',
+//         text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
+//     },
 
-    {
-        name: 'Emilie Beach',
-        date: '01/09/2021',
-        text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-    },
+//     {
+//         name: 'Emilie Beach',
+//         date: '01/09/2021',
+//         text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
+//     },
     
-    {
-        name: 'Miles Acosta',
-        date: '12/20/2020',
-        text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    }
+//     {
+//         name: 'Miles Acosta',
+//         date: '12/20/2020',
+//         text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//     }
 
-]
+// ]
+
+let comments =[]
+
+let apiKey = "cb39efe6-7135-448f-8ccc-0d37be500d2c"
+let liveComments = `https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`
+
+axios.get(liveComments).then(result => {
+    console.log(result.data);
+    // comments = result.data;
+    displayAllComments(result.data);
+})
+
+
+const updatedDate = (realDate) => {
+    let currentDate = new Date();
+    let month = currentDate.getMonth();
+    let day = currentDate.getDate();
+    let year = currentDate.getFullYear();
+    const fullDate = month + "/" + day + "/" + year;
+    return fullDate
+    console.log(updatedDate);
+}
+
+
 
 const commentSection = document.querySelector(".comments-section");
-
-function displayAllComments(){
-    defaultComment.forEach((comment) => {
+function displayAllComments(array){
+    array.forEach((comment) => {
         const viewContainer = document.createElement('div');
         viewContainer.classList.add("view-container");
         commentSection.appendChild(viewContainer);
@@ -45,13 +68,13 @@ function displayAllComments(){
     
         const viewDate = document.createElement('p');
         viewDate.classList.add("view-date");
-        viewDate.innerText = comment.date;
+        viewDate.innerText = comment.fullDate;
         viewComments.appendChild(viewDate);
     
     
         const viewText = document.createElement('p');
         viewText.classList.add("view-text");
-        viewText.innerText = comment.text;
+        viewText.innerText = comment.comment;
         viewComments.appendChild(viewText);
     }
     )
@@ -84,7 +107,7 @@ commentsForm.addEventListener('submit', (b)  => {
     b.target.reset();
 })
 
-displayAllComments();
+// displayAllComments();
 
 
 
